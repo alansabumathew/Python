c1232b41d398bf5266d2174dec9ed8a18cccb00e
Alan Sabu Mathew <alanmathew349@gmail.com>
2023-03-11 12:38:26 +0530

    Mad libs added

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+noun=input("Enter a name:")
+verb=input("Verb the ends with 'ing':")
+noun2=input("Enter name of a place:")
+noun3=input("Entern name of a dish")
+
+madlibs=f"Hello welcome to {noun},here we will be {verb} about {noun} and we will have a lot {noun3}."
+
+print(madlibs)
